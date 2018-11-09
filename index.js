b9eddb07a48e780b7766ede78e8f80db572dc704
Courtney Persons <courtneypersons@Courtneys-MBP.PK5001Z>
2018-11-09 13:07:03 -0500

    final quiz app

--- index.js
+++ index.js
@@ -23,61 +23,61 @@ const quizQuestions = [
       correct: "Andrew Bynum"
     },
   
-    //   {
-    //   question: "who scored the most points in an nba game?",
+       {
+      question: "who scored the most points in an nba game?",
   
-    //   answers: ["Kobe Bryant", "Wilt Chamberlain", "Magic Johnson", "MIcheal Jordan"],
+      answers: ["Kobe Bryant", "Wilt Chamberlain", "Magic Johnson", "MIcheal Jordan"],
   
-    //   correct: "Wilt Chamberlain"
-    //  },
+      correct: "Wilt Chamberlain"
+     },
     
-    //  {
-    //  question: "How many rings does Bill Russel have?",
+     {
+     question: "How many rings does Bill Russel have?",
   
-    //   answers: [6, 5, 8, 11],
+      answers: [6, 5, 8, 11],
   
-    //   correct: 11
-    //  },
-    //  {
-    //  question: "Most minutes played in nba history by who?",
+      correct: 11
+     },
+     {
+     question: "Most minutes played in nba history by who?",
      
-    //  answers: ["Kareem Abdul-Jabbar", "Karl Malone", "Dirk Nowitzki", "Kevin Garnett"],
+     answers: ["Kareem Abdul-Jabbar", "Karl Malone", "Dirk Nowitzki", "Kevin Garnett"],
      
-    //  correct: "Kareem Abdul-Jabbar"
-    // },
-    // {
-    //  question: "who has the most 3 pointers made in nba history",
+     correct: "Kareem Abdul-Jabbar"
+    },
+    {
+     question: "who has the most 3 pointers made in nba history",
      
-    //  answers: ["Stephen Curry", "Reggie Miller", "Ray Allen" , "Jason Terry" ],
+     answers: ["Stephen Curry", "Reggie Miller", "Ray Allen" , "Jason Terry" ],
      
-    //  correct: "Ray Allen"
-    // },
-    // {
-    // question: "Which player was NOT drafted out of high school?",
+     correct: "Ray Allen"
+    },
+    {
+    question: "Which player was NOT drafted out of high school?",
         
-    // answers: ['Kobe Bryant', 'Kevin Garnett', 'LeBron James' , 'Shaquille O\'neal' ],
+    answers: ['Kobe Bryant', 'Kevin Garnett', 'LeBron James' , 'Shaquille O\'neal' ],
         
-    // correct: 'Shaquille O\'neal'
-    // },
-    // {
-    //     question: "'Which state has the most NBA teams?'",
+    correct: 'Shaquille O\'neal'
+    },
+    {
+        question: "'Which state has the most NBA teams?'",
             
-    //     answers: ["New York", "California", "Texas" , "Florida" ],
+        answers: ["New York", "California", "Texas" , "Florida" ],
             
-    //     correct: "California"
-    // },
-    // {
-    //     question: "Who has the highest points per game in NBA playoff history?",
+        correct: "California"
+    },
+    {
+        question: "Who has the highest points per game in NBA playoff history?",
             
-    //     answers: ["Jerry West", "Allen Iverson", "Lebron James" , "Michael Jordan" ],
+        answers: ["Jerry West", "Allen Iverson", "Lebron James" , "Michael Jordan" ],
             
-    //     correct: "Michael Jordan"
-    // },
-    // {
-    //     question: "what player has the most nba playoff wins",
+        correct: "Michael Jordan"
+    },
+    {
+        question: "what player has the most nba playoff wins",
             
-    //     answers: ["Tim Duncan", "Michael Jordan", "Derek Fisher" , "Lebron James" ],
+        answers: ["Tim Duncan", "Michael Jordan", "Derek Fisher" , "Lebron James" ],
             
-    //     correct: "Derek Fisher"
-    // }           
+        correct: "Derek Fisher"
+    }           
   ];
